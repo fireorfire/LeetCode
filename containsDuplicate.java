b7f07405014074c9688d7b7a0b48384c0b969728
James Lau <liuzijianhuolong@163.com>
2015-06-06 11:22:39 -0700

    contains duplicate

---
+++ containsDuplicate.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> hash = new HashSet<>();
+        for(int x : nums) {
+            if(hash.contains(x)) return true;
+            hash.add(x);
+        }
+        return false;
+    }
+}
